1f6ad4bff3ffbc5810b0ba20065a34d73d12be51
mtatarek <maciej.tatarek93@gmail.com>
2017-12-09 16:15:03 +0100

    Add tcp_flag in app

--- pytest.py
+++ pytest.py
@@ -1,15 +1,21 @@
 import sys
 import os
+import requests
+import json
 
-with open('/var/tcp_dump/test', 'r') as f:
-	data = f.read()
-print data
-if data != "0":
-    with open('/var/tcp_dump/test', 'w') as f:
-    	f.write("0")
-	command = "sudo timeout 100 tcpdump >> /var/tcp_dump/tcptest"
-	os.system(command)
-	sys.exit(0)
-else:
-	sys.exit(0)
+if __name__ == '__main__':
+
+	r = requests.get('http://0.0.0.0:5050/tcp_flag')
+	json_data = json.loads(r.text)
+	tcp_flag = json_data["tcp_flag"]
+	print tcp_flag
+
+	#TODO: subprocess
+
+	if tcp_flag == True:
+		command = "echo test > test.txt"
+		os.system(command)
+		sys.exit(0)
+	else:
+		sys.exit(0)
 
--- server.py
+++ server.py
@@ -1,23 +1,34 @@
 from flask import Flask, render_template
-from flask import request
+from flask import request, jsonify
 import sys, os
 from threading import Timer
 
 app = Flask(__name__)
+tcp_dump = False
+
+@app.route("/", methods=['GET'])
+def index():
+    return  render_template('index.html')
 
 @app.route("/redirect", methods=['GET'])
 def redirect():
-    with open('test', 'w') as f:
-        f.write("10")
-    return render_template('redirect.html')
+    global tcp_dump
+    if tcp_dump:
+        return "tcp dump in use, please try again in few minutes"
+    else:
+        tcp_dump = True
+        return render_template('redirect.html')
 
 @app.route("/dumping", methods=['GET'])
 def dumping_data():
     return render_template('dumping.html')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/tcp_flag', methods=['GET', 'POST'])
+def tcp_flag():
+    if request.method == "GET":
+        return jsonify({"tcp_flag": tcp_dump})
+    # elif request.method == "POST":
+    #     tcp_dump
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050)
+    app.run(host='0.0.0.0', port=5050, debug=True)
