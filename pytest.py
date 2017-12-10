ac3bc06634d5c089a71c391303de835c086c72f0
mtatarek <maciej.tatarek93@gmail.com>
2017-12-10 16:21:23 +0100

    add get_state function

--- pytest.py
+++ pytest.py
@@ -2,28 +2,34 @@ import subprocess
 import requests
 import json
 import time
+from threading import Thread
+
+def get_state(delay):
+	try:
+		time.sleep(delay)
+		r = requests.get('http://0.0.0.0:5050/tcp_flag')
+		json_data = json.loads(r.text)
+		tcp_flag = json_data["tcp_flag"]
+		print "[Server] Connected!"
+		return tcp_flag
+	except Exception, e:
+		print "[Server Error] Can't connect with server"
+
 
 if __name__ == '__main__':
-	while True:
-		try:
-			time.sleep(5)
-			r = requests.get('http://0.0.0.0:5050/tcp_flag')
-			json_data = json.loads(r.text)
-			tcp_flag = json_data["tcp_flag"]
-			print "Connected with server"
-			print "tcp_flag: ", tcp_flag
 
-			if tcp_flag == True:
-				filename = "test.txt"
-				dump_time = "5"
-				command = "timeout {} tcpdump > {}".format(dump_time, filename)
-				subprocess.call(command, shell=True)
-				requests.post('http://0.0.0.0:5050/tcp_flag')
-			# Cron
-			# 	sys.exit(0)
-			# else:
-			# 	sys.exit(0)
-		except Exception, e:
-			print "[Server Error] Can't connect with server"
+	while True:
+
+		tcp_flag = get_state(5)
+		if tcp_flag == True:
+			filename = "test.txt"
+			dump_time = "5"
+			command = "timeout {} tcpdump > {}".format(dump_time, filename)
+			subprocess.call(command, shell=True)
+			requests.post('http://0.0.0.0:5050/tcp_flag')
+
+
+
+
 
 
