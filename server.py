cf6bacaa03e530e451f7adc57271b1fffbf233ea
mtatarek <maciej.tatarek93@gmail.com>
2017-12-11 21:13:22 +0100

    Add start stop download mechanics

--- server.py
+++ server.py
@@ -1,40 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from flask import request, jsonify
 
 app = Flask(__name__)
 tcp_dump = False
+
+                #Main part
 
 @app.route("/", methods=['GET'])
 def index():
     return  render_template('index.html')
 
 @app.route("/redirect", methods=['GET'])
-def redirect():
+def redirect_delay():
     global tcp_dump
     if tcp_dump:
         return "tcp dump in use, please try again in few minutes"
     else:
         tcp_dump = True
         return render_template('redirect.html')
-
-@app.route("/start_dumping", methods=['GET', 'POST'])
-def start_dumping():
-    global tcp_dump
-    if request.method == 'GET':
-        return render_template('start_dumping.html')
-    else:
-        ans = request.form.keys()
-        if ans[0] == 'start':
-            tcp_dump = True
-            return render_template('stop_dumping.html')
-
-@app.route("/download", methods=['POST'])
-def stop_dumping():
-    global tcp_dump
-    ans = request.form.keys()
-    if ans[0] == 'stop':
-        tcp_dump = False
-        return render_template('download.html')
 
 @app.route('/tcp_flag', methods=['GET', 'POST'])
 def tcp_flag():
@@ -44,6 +27,32 @@ def tcp_flag():
     elif request.method == "POST":
         tcp_dump = False
         return jsonify({"tcp_flag": tcp_dump})
+
+                    #Functionss
+
+@app.route("/start_dumping", methods=['GET', 'POST'])
+def start_dumping():
+    global tcp_dump
+    if request.method == 'GET':
+        return render_template('start_dumping.html')
+    else:
+        # ans = request.form.keys()
+        # if ans[0] == 'start':
+        #     tcp_dump = True
+        return redirect(url_for('stop_dumping'))
+
+@app.route("/stop_dumping", methods=['GET', 'POST'])
+def stop_dumping():
+    global tcp_dump
+    if request.method == 'GET':
+        return render_template('stop_dumping.html')
+    else:
+        return redirect(url_for('download'))
+
+@app.route("/download", methods=['GET', 'POST'])
+def download():
+    return render_template('download.html')
+
 
 
 if __name__ == '__main__':
